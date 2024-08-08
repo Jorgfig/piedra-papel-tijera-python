a5fa2e9a6c07f8b8a84f0f7add0bae0a1351b316
JorgeFigueroa <figueroajorgear@gmail.com>
2024-08-08 10:25:56 -0300

    Se realizan modifaciones en el juego

--- piedra-papel-tijera.py
+++ piedra-papel-tijera.py
@@ -2,13 +2,12 @@ nombre1 = input("¿Cómo se llama el jugador 1?: ")
 nombre2 = input("¿Cómo se llama el jugador 2?: ")
 
 
-#Queda pendiente colocar el nombre del jugador 1 y 2 y ver lo de las minusculas sumado al bucle
+jugador1 = input (f"¡Hola {nombre1} ¿Qué elegis piedra, papel o tijera?: ")
+jugador2 = input (f"¡Hola {nombre2} ¿Qué elegis piedra, papel o tijera?: ")
+print (jugador1.lower())
+print (jugador2.lower())
 
-jugador1 = input ("¡Hola jugador1 ¿Qué elegis: Piedra, papel o tijera?: ")
-jugador2 = input ("¡Hola jugador2 ¿Qué elegis: Piedra, papel o tijera?: ")
-
-
-
+ 
 condicion1 = jugador1 == "piedra" and jugador2 == "tijera"
 condicion2 = jugador1 == "papel" and jugador2 == "piedra"
 condicion3 = jugador1 == "tijera" and jugador2 =="papel"
@@ -17,8 +16,10 @@ if jugador1 == jugador2:
     print ("¡Ha sido un empate!")
 
 elif condicion1 or condicion2 or condicion3:
-    print ("Ha ganado: ", nombre1)
+    print ("Ha ganado: ",nombre1)
 
 else:
-    print ("Ha ganado: ", nombre2)
+    print ("Ha ganado: ",nombre2)
+
+
 
